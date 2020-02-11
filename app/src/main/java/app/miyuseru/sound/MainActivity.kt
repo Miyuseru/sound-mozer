952e925bf86c074a269256f10164e558c0867885
miyuu kuwana <miyuu13k@gmail.com>
2020-02-12 03:21:35 +0900

    sound3

--- app/src/main/java/app/miyuseru/sound/MainActivity.kt
+++ app/src/main/java/app/miyuseru/sound/MainActivity.kt
@@ -15,6 +15,9 @@ class MainActivity : AppCompatActivity() {
 
 
         val  drumSound = MediaPlayer.create(this,R.raw.drum_sound)
+        val  pianoSound = MediaPlayer.create(this,R.raw.piano_sound)
+        val  guitarSound = MediaPlayer.create(this,R.raw.guitar_sound)
+
 
 
         drumImage.setOnTouchListener{ view, event ->
@@ -33,6 +36,42 @@ class MainActivity : AppCompatActivity() {
 
                 true
             }
+
+
+        pianoimage.setOnTouchListener{ view, event ->
+
+            if(event.action == MotionEvent.ACTION_DOWN) {
+                pianoimage.setImageResource(R.drawable.piano_playing_image)
+
+                pianoSound.seekTo(0)
+                pianoSound.start()
+
+            }
+            else if(event.action == MotionEvent.ACTION_UP){
+                pianoimage.setImageResource(R.drawable.piano_image)
+            }
+
+
+            true
+        }
+
+         guitarImage.setOnTouchListener{ view, event ->
+
+            if(event.action == MotionEvent.ACTION_DOWN) {
+                guitarImage.setImageResource(R.drawable.guitar_playing_image)
+
+                guitarSound.seekTo(0)
+                guitarSound.start()
+
+            }
+            else if(event.action == MotionEvent.ACTION_UP){
+                guitarImage.setImageResource(R.drawable.guitar_image)
+            }
+
+
+            true
+        }
+
 
 
 
